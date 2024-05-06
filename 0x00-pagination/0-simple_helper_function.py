df9f622352a7391545248b5072f6835e266c0721
nezhataghy <nezhataghy@gmail.com>
2024-05-06 21:55:03 +0000

    Simple helper function

---
+++ 0x00-pagination/0-simple_helper_function.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""Simple helper function
+"""
+
+
+def index_range(page: int, page_size: int) -> tuple:
+    """
+    Return a tuple of size two containing a start index and an end index
+    """
+    next_page: int = page * page_size
+    prev_page: int = next_page - page_size
+    return (prev_page, next_page)
